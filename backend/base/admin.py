72dff4833aad6ba72a724b87de913bdfb196fbe2
Dmytrenko-Roman <savitarreus@gmail.com>
2022-07-14 15:25:08 +0300

    ❄️ Added all models ❄️

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from base.models import CustomUser, Car
+from base.models import *
 
 
 admin.site.register(CustomUser)
 admin.site.register(Car)
+admin.site.register(Review)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
--- backend/base/models.py
+++ backend/base/models.py
@@ -29,3 +29,89 @@ class Car(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Review(models.Model):
+    _id = models.AutoField(primary_key=True, editable=False)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    rating = models.IntegerField(null=True, blank=True, default=0)
+    comment = models.TextField(null=True, blank=True)
+
+    user = models.ForeignKey(
+        to=CustomUser,
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    car = models.ForeignKey(
+        to=Car,
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+
+    def __str__(self):
+        return str(self.rating)
+
+
+class Order(models.Model):
+    _id = models.AutoField(primary_key=True, editable=False)
+    payment_method = models.CharField(max_length=200, null=True, blank=True)
+    tax_price = models.DecimalField(max_digits=10, decimal_places=2)
+    shipping_price = models.DecimalField(max_digits=10, decimal_places=2)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_paid = models.BooleanField(default=False)
+    paid_at = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+    is_delivered = models.BooleanField(default=False)
+    delivered_at = models.DateTimeField(
+        auto_now_add=False, null=True, blank=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    user = models.ForeignKey(
+        to=CustomUser,
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+
+    def __str__(self):
+        return str(self.created_at)
+
+
+class OrderItem(models.Model):
+    _id = models.AutoField(primary_key=True, editable=False)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    qty = models.IntegerField(null=True, blank=True, default=0)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    car = models.ForeignKey(
+        to=Car,
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    # image =
+    order = models.ForeignKey(
+        to=Order,
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class ShippingAddress(models.Model):
+    _id = models.AutoField(primary_key=True, editable=False)
+    address = models.CharField(max_length=200, null=True, blank=True)
+    city = models.CharField(max_length=200, null=True, blank=True)
+    postal_code = models.CharField(max_length=200, null=True, blank=True)
+    country = models.CharField(max_length=200, null=True, blank=True)
+    shipping_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    order = models.OneToOneField(
+        to=Order,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.address
