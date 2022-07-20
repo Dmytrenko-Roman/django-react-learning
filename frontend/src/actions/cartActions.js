482c17e07e60901b6e4d3f1fc8dd36f2a4e8ab80
Dmytrenko-Roman <savitarreus@gmail.com>
2022-07-20 20:48:16 +0300

    ❄️ Cart implementation ❄️

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { CART_ADD_ITEM } from '../constants/cartConstants';
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants';
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
     const { data } = await axios.get(`http://127.0.0.1:8000/api/cars/${id}`);
@@ -12,8 +12,17 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
             image: data.image,
             price: data.price,
             count_in_stock: data.count_in_stock,
-            qty
+            qty,
         }
+    });
+
+    localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
+};
+
+export const removeFromCart = (id) => (dispatch, getState) => {
+    dispatch({
+        type: CART_REMOVE_ITEM,
+        payload: id,
     });
 
     localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,52 +1,33 @@
-import { CART_ADD_ITEM } from '../constants/cartConstants';
-
-// const checkItem = (state, action) => {
-//     const item = action.payload;
-//     const existItem = state.cartItems.find((x) => x.car === item.car);
-
-//     if (existItem) {
-//         return {
-//             ...state,
-//             cartItems: state.cartItems.map(
-//                 x => x.car === existItem.car ? item : x
-//             )
-//         };
-//     } else {
-//         return {
-//             ...state,
-//             cartItems: [...state.cartItems, item]
-//         };
-//     }
-// };
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants';
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
-    // const actions = {
-    //     [CART_ADD_ITEM]: checkItem(state, action),
-    // };
-
-    // const res = actions[action.type];
-    // return (res !== undefined) ? res : state;
-
-    switch (action.type) {
-        case CART_ADD_ITEM: {
-            const item = action.payload
-            const existItem = state.cartItems.find(x => x.product === item.product)
+    const actions = {
+        [CART_ADD_ITEM]: () => {
+            const item = action.payload;
+            const existItem = state.cartItems.find((x) => x.car === item.car);
 
             if (existItem) {
                 return {
                     ...state,
-                    cartItems: state.cartItems.map(x =>
-                        x.product === existItem.product ? item : x)
-                }
-
+                    cartItems: state.cartItems.map(
+                        x => x.car === existItem.car ? item : x
+                    )
+                };
             } else {
                 return {
                     ...state,
                     cartItems: [...state.cartItems, item]
-                }
+                };
             }
+        },
+        [CART_REMOVE_ITEM]: () => {
+            return {
+                ...state,
+                cartItems: state.cartItems.filter(x => x.car !== action.payload)
+            };
         }
-        default:
-            return state
-    }
+    };
+
+    const res = actions[action.type];
+    return (res !== undefined) ? res() : state;
 };
