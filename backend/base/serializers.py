0de43a950b02d449b4a4c3cc665ff5df821ddd1e
Dmytrenko-Roman <savitarreus@gmail.com>
2022-07-22 09:23:16 +0300

    ❄️ User token serializer ❄️

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -1,17 +1,8 @@
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from base.models import Car, CustomUser
-
-
-class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
-    def validate(self, attrs):
-        data = super().validate(attrs)
-
-        data["username"] = self.user.username
-
-        return data
-
 
 class CarSerializer(serializers.ModelSerializer):
     class Meta:
@@ -20,6 +11,47 @@ class CarSerializer(serializers.ModelSerializer):
 
 
 class UserSerializer(serializers.ModelSerializer):
+    _id = serializers.SerializerMethodField(read_only=True)
+    name = serializers.SerializerMethodField(read_only=True)
+    is_admin = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = CustomUser
-        fields = ["id", "username", "email"]
+        fields = ["id", "_id", "username", "email", "name", "is_admin"]
+
+    def get__id(self, obj):
+        return obj.id
+
+    def get_name(self, obj):
+        name = obj.first_name
+        if name == "":
+            name = obj.email
+
+        return name
+    
+    def get_is_admin(self, obj):
+        return obj.is_staff
+
+
+class UserSerializerWithToken(UserSerializer):
+    token = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ["id", "_id", "username", "email", "name", "is_admin", "token"]
+    
+    def get_token(self, obj):
+        token = RefreshToken.for_user(obj)
+
+        return str(token.access_token)
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        serializer = UserSerializerWithToken(self.user).data
+        for key, value in serializer.items():
+            data[key] = value
+
+        return data
